cafea84cf62f23529c17b7a0eacaf2fb5d73e2a8
VidhyalakshmiKrishnan <vidhyalakshmikrishnan15@gmail.com>
2025-04-19 02:33:55 +0200

    creating 3 EC2 Instances

--- v2-ec2.tf
+++ v2-ec2.tf
@@ -7,8 +7,13 @@ resource "aws_instance" "demo-server" {
 ami="ami-08f78cb3cc8a4578e"
 instance_type = "t3.micro"
 key_name = "dpp"
-security_groups = [ "demo-sg" ]
-subnet_id = aws_subnet.dpp-public-subnet-01
+//security_groups = [ "demo-sg" ]
+subnet_id = aws_subnet.dpp-public-subnet-01.id
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+for_each = [ "Jenkins-master", "Build-slave" ,"Ansible" ]
+tags = {
+  name = "${each.key}" 
+}
 
 }
 
@@ -53,7 +58,7 @@ resource "aws_subnet" "dpp-public-subnet-01" {
   
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.1.0/24"
-  availability_zone = "eu-north-1"
+  availability_zone = "eu-north-1a"
   map_public_ip_on_launch = true
   tags = {
     name= "dpp-public-subnet-01"
@@ -64,7 +69,7 @@ resource "aws_subnet" "dpp-public-subnet-02" {
   
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.2.0/24"
-  availability_zone = "eu-north-1"
+  availability_zone = "eu-north-1b"
   map_public_ip_on_launch = true
   tags = {
     name= "dpp-public-subnet-02"
@@ -82,7 +87,7 @@ resource "aws_internet_gateway" "dpp-igw" {
 
 resource "aws_route_table" "dpp-public-rt" {
     vpc_id = aws_vpc.dpp-vpc.id
-    route = {
+    route  {
         cidr_block="0.0.0.0/0"
         gateway_id= aws_internet_gateway.dpp-igw.id
     }
@@ -95,11 +100,11 @@ resource "aws_route_table" "dpp-public-rt" {
 resource "aws_route_table_association" "dpp-rta-public-subnet01" {
 
     route_table_id = aws_route_table.dpp-public-rt.id 
-    subnet_id=aws_subnet.dpp-public-subnet-01
+    subnet_id=aws_subnet.dpp-public-subnet-01.id
 }
 
 resource "aws_route_table_association" "dpp-rta-public-subnet02" {
 
     route_table_id = aws_route_table.dpp-public-rt.id 
-    subnet_id=aws_subnet.dpp-public-subnet-02
+    subnet_id=aws_subnet.dpp-public-subnet-02.id
 }
